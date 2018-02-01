ffa3feebe6138b4df18ece267e506cf28bdd0614
aleix214 <35691131+aleix214@users.noreply.github.com>
2018-02-01 12:43:36 +0100

    Update nveces_01.py

--- nveces_01.py
+++ nveces_01.py
@@ -2,11 +2,15 @@
 
 num1 = int(input("Escribe un número: "))
 
-i=0
-
+suma=0
+suma=0
 
 while (i<num1):
 	aux= int(input("Escribe un número: "))
 	if (aux>0):
 		i+=1
+	suma=suma+num1
+	
+print "El total de la suma de los numeros positivos es:", str(j) + "."
+
 
