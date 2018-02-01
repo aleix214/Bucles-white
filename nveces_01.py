df1d461db43e05306b703e79370ba4687af09646
ism47893134 <ism47893134@a13.informatica.escoladeltreball.org>
2018-02-01 13:06:12 +0100

    first commit

--- nveces_01.py
+++ nveces_01.py
@@ -3,8 +3,13 @@
 num1 = int(input("Escribe un número: "))
 
 i=0
+suma=0
 
 while (i<num1):
-	i=i+1
-	print("Introduce un número")
+	aux= int(input("Escribe un número: "))
+	if (aux>0):
+		i+=1
+		suma=suma+num1
+	
+print "El total de la suma de los numeros positivos es:", str(suma) + "."
 
