3ae717fd798e5205b91b1dcb88143873978b3c93
aleix214 <35691131+aleix214@users.noreply.github.com>
2018-02-08 13:12:27 +0100

    Update mayorK06.py

--- mayorK06.py
+++ mayorK06.py
@@ -1,10 +1,10 @@
 #coding: utf­8
 
-num1 = int(input("Escribe el límite: "))
+num1 = float(input("Escribe el límite: "))
 suma=0
 
 while num1>0:
-	aux = int(input("Escribe otro: "))
+	aux = float(input("Escribe otro: "))
 	suma=aux+suma
 	if (suma>num1):
 		print "Ha superado el límite. La suma de los números introducidos es", str(suma) + "." 
