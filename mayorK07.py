d167591b3a6b58f4c74d0acdffb2de669095f6bf
aleix214 <35691131+aleix214@users.noreply.github.com>
2018-02-15 12:58:02 +0100

    Update mayorK07.py

--- mayorK07.py
+++ mayorK07.py
@@ -1,12 +1,21 @@
 #coding: utf­8
+min = int(input("Escriba un número: "))
+max = int(input("Escriba un número mayor que " + str(min) + ": "))
+while min >= max:
+    max = int(input(str(max) + " no es mayor que " + str(min) + ".Inténtelo de nuevo: "))
 
-num1 = int(input("Escribe un número: "))
-num2 = int(input("Escribe un número mayor que el anterior: "))
+num = float(input("Escriba un número entre " + str(min) + " y " + str(max) + ": "))
+contador = 0
 
-x=0
+while min <= num <= max:
+    contador += 1
+    num = float(input("Escriba un número entre " + str(min) + " y " + str(max) + ": "))
 
-while (num1<num2):
-	aux = int(input("Escribe el número: "))
-	x=x+aux
-	if (aux>num2) and (aux<num2):
-		print "jeje"
+print()
+if contador == 0:
+    print("No ha escrito ningún número entre", min, "y", str(max) + ".")
+elif contador == 1:
+    print("Ha escrito 1 número entre", min, "y", str(max) + ".")
+else:
+    print("Ha escrito", contador, "números entre", min, "y", str(max) + ".")
+print("Programa terminado")
